ba07fb3ae36438b25df72ecf31815e883500b5e5
Johnny Kingi / 10005351 <10005351@bcs.net.nz>
2018-04-13 13:26:14 +1200

    Q3 started, main functionality working, need to add checking for parenthesis and user input

---
+++ Q2_Palindromes/Q2_Palindromes/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Q2_Palindromes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack _stack = new Stack();
+            Stack _stack2 = new Stack();
+            Stack _stack3 = new Stack();
+            palindromeCheck(_stack, _stack2, _stack3);
+            printPalindrome(_stack);
+        }
+
+        static void palindromeCheck(Stack _stack, Stack _stack2, Stack _stack3)
+        {
+            Console.WriteLine("Enter a pharse to check if it is a palindrome");
+            var input = Console.ReadLine();
+
+            input = input.ToLower(); //ignore capitlization
+            input = Regex.Replace(input, @"[^\w]", string.Empty); //remove all Punctuation
+
+            foreach (char i in input)//adding chars of input individually into stack array 
+            {
+                _stack.Push(i);
+            }
+            Console.WriteLine("Stack1:  ");
+            foreach (char item in _stack)//writing stack to check
+            {
+                Console.Write(item);
+            }
+
+            _stack2 = new Stack(_stack.ToArray());//Copyin Stack to _stack2
+            Console.WriteLine();
+
+            Console.WriteLine("Stack2:  ");
+            foreach (char item in _stack2)//writing stack to check
+            {
+                Console.Write(item);
+            }
+
+            Console.WriteLine();
+
+            foreach (var item in _stack2)//using _stack3 to take items back into stack
+            {
+                _stack3.Push(item);
+            }
+
+            printPalindrome(_stack);
+
+            Console.WriteLine();
+
+            while (_stack2.Count > 0)
+            {
+                var peek1 = _stack2.Peek();
+                var peek2 = _stack3.Peek();
+                //Console.WriteLine("{0}  {1}",peek1,peek2);
+                if (peek1.ToString() == peek2.ToString())
+                {
+                    _stack2.Pop();
+                    _stack3.Pop();
+                }
+                else
+                {
+                    _stack2.Clear();
+                }
+            }
+
+            Console.WriteLine();
+
+            if (_stack3.Count == 0)
+            {
+                Console.WriteLine("Yes this is a Palindrome");
+
+            }
+            else
+            {
+                Console.WriteLine("Nope not a Palindrome");
+            }
+
+            //Console.Write(item);
+            /*
+            if (_stack2.Equals(_stack3) == true)
+            {
+                Console.WriteLine("Yes this string is a Palindrome");
+            }
+            else
+            {
+                Console.WriteLine("No string is not a Palindrome");
+            }
+            //Console.WriteLine(compare);*/
+
+            Console.ReadLine();
+        }
+
+        static void printPalindrome(Stack _Stack)
+        {
+            Stack _stack = new Stack();
+            foreach (char item in _stack)//writing stack to check
+            {
+                Console.Write(item);
+            }
+
+        }
+    }
+
+    
+}
---
+++ Q3_Infix_Postfix/Q3_Infix_Postfix/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrecV2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Please input string to be converted from infix to postfix:\n");
+            string input = "2+3*2/6+18";
+            string postfix = " ";
+
+            Stack s = new Stack();
+            //s.Push(')');
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (char.IsNumber(c) == true)
+                {
+                    postfix += c;
+                }
+
+                else if (IsOperator(c) == true)
+                {
+                    if (s.Count <= 0)
+                    {
+                        Console.WriteLine("Count 0 - pushing:  " + c);
+                        s.Push(c);
+                    }
+                    else
+                    {
+                        var d = s.Peek().ToString();
+                        char op2 = d[0];//top of stack to char
+                        int Prec = 0;
+                        Console.WriteLine("top of stack:  " + op2);
+                        Console.ReadLine();
+
+                        if (op2 == '*' || op2 == '/')
+                        {
+                            Prec = 2;
+                        }
+                        else
+                        {
+                            Prec = 0;
+                        }
+
+                        if (Prec == 2)
+                        {
+                            if (c == '+' || c == '-')
+                            {
+                                postfix += s.Pop();
+                                i--;
+                            }
+                            else
+                            {
+                                Console.WriteLine("Top of stack: ", +op2);
+                                postfix += s.Pop();
+                                i--;
+                            }
+                        }
+
+
+                        else
+                        {
+                            if (c == '+' || c == '-')
+                            {
+                                Console.WriteLine("Top of stack: ", +op2);
+                                postfix += s.Pop();
+                                s.Push(c);
+                            }
+                            else
+                                s.Push(c);
+
+                        }
+                    }
+
+                }
+            }
+
+            int len = s.Count;
+
+            for (int i = 0; i < len; i++)
+                postfix += s.Pop();
+
+            Console.WriteLine();
+            foreach (var item in s)
+            {
+                Console.WriteLine("Stack:  " + item);
+            }
+
+            Console.WriteLine("final Postfix: " + postfix);
+            Console.ReadLine();
+
+        }
+        public static bool IsOperator(char c)
+        {
+            if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^' || c == '%')
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
